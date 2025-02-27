cf43f13b7f3b5657b5f6110b587dcb34e0346add
chcaya <cayc2401@usherbrooke.ca>
2025-02-27 22:44:07 +0000

    Added images and upgraded inference results

--- inference.py
+++ inference.py
@@ -8,7 +8,22 @@ model = main.deepforest()
 # Load a pretrained tree detection model from Hugging Face
 model.load_model(model_name="weecology/deepforest-tree", revision="main")
 
-# sample_image_path = get_data("OSBS_029.png")
-sample_image_path = get_data("/home/docker/python_docker/scaned_img.png")
-img = model.predict_image(path=sample_image_path)
-plot_results(img)
+# image_path = get_data("OSBS_029.png")
+image_path = get_data("/home/docker/python_docker/images/simulation/scaned_img_65m.png")
+# img = model.predict_image(path=image_path)
+# raster = model.predict_tile(image_path, patch_size=800, patch_overlap=0.25)
+# plot_results(raster)
+
+boxes = model.predict_image(path=image_path, return_plot=False)
+
+boxes["area"] = (boxes["xmax"] - boxes["xmin"]) * (boxes["ymax"] - boxes["ymin"])
+boxes["x"] = (boxes["xmax"] - boxes["xmin"])/2.0 + boxes["xmin"]
+boxes["y"] = (boxes["ymax"] - boxes["ymin"])/2.0 + boxes["ymin"]
+
+max_area_idx = boxes["area"].idxmax()
+center_x = int(boxes.loc[max_area_idx, "x"])
+center_y = int(boxes.loc[max_area_idx, "y"])
+
+# Output bounding boxes
+print(boxes)
+print(f"Center of the Largest Bounding Box: ({center_x}, {center_y})")
