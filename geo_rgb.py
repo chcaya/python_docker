2ffb94edbd4e9db7676d2d1089bb915dd1aa7dcd
chcaya <cayc2401@usherbrooke.ca>
2025-03-13 14:39:17 -0400

    Cleaned up geo_rgb.py

--- geo_rgb.py
+++ geo_rgb.py
@@ -48,27 +48,36 @@ def rotate_corners(_corners, _yaw):
     ]
     return corners_rotated
 
-HEIGHT = 65
-# HEIGHT = 78
+def get_corners(_width, _height):
+    return [
+        (-_width / 2.0, _height / 2.0),  # Top-left
+        (_width / 2.0, _height / 2.0),   # Top-right
+        (-_width / 2.0, -_height / 2.0), # Bottom-left
+        (_width / 2.0, -_height / 2.0),  # Bottom-right
+    ]
+
+def compute_bbox(_corners):
+    max_x = max(corner[0] for corner in _corners)
+    min_x = min(corner[0] for corner in _corners)
+    max_y = max(corner[1] for corner in _corners)
+    min_y = min(corner[1] for corner in _corners)
+
+    x_size = max_x - min_x
+    y_size = max_y - min_y
+
+    return x_size, y_size
+
+
+HEIGHT = 65 # Max accuracy on tree
+# HEIGHT = 78 # Real height
 
 CAM_FOV_H = 87
 # CAM_FOV_V = 58
 CAM_FOV_V = 48.9 # In sim
 
-# CENTER_LAT = 45.0  # Latitude in degrees
-# CENTER_LONG = -72.0  # Longitude in degrees
-# CENTER_LAT = 45.3777902  # Latitude in degrees
-# CENTER_LONG = -71.9401419  # Longitude in degrees
-# CENTER_LAT = 45.37783  # Latitude in degrees
-# CENTER_LONG = -71.94017  # Longitude in degrees
-# CENTER_LAT = 45.37769  # Latitude in degrees
-# CENTER_LONG = -71.94007  # Longitude in degrees
-# CENTER_LAT = 45.3777901  # Latitude in degrees
-# CENTER_LONG = -71.940144  # Longitude in degrees
-# YAW = math.radians(5.558) - math.radians(90)
-
 CENTER_LAT = 45.3777901  # Latitude in degrees
 CENTER_LONG = -71.9401421  # Longitude in degrees
+# CENTER_LONG = -71.940125  # Longitude in degrees
 YAW = math.radians(5.605) - math.radians(90)
 
 image_width_meters = compute_img_size(CAM_FOV_H, HEIGHT)
@@ -90,99 +99,42 @@ image_height_pixels = png_image.shape[0]
 image_width_pixels = png_image.shape[1]
 
 # Convert the rotated local coordinates to geographic coordinates
-corners_meters = [
-    (-image_width_meters / 2.0, image_height_meters / 2.0),  # Top-left
-    (image_width_meters / 2.0, image_height_meters / 2.0),   # Top-right
-    (-image_width_meters / 2.0, -image_height_meters / 2.0), # Bottom-left
-    (image_width_meters / 2.0, -image_height_meters / 2.0),  # Bottom-right
-]
-
+corners_meters = get_corners(image_width_meters, image_height_meters)
 corners_meters_rotated = rotate_corners(corners_meters, YAW)
-
 corners_geo = [
     (CENTER_LONG + (x / meters_per_degree_lon), CENTER_LAT + (y / meters_per_degree_lat))
     for x, y in corners_meters_rotated
 ]
-
-print("Geo corners: " + str(corners_geo))
+img_long, img_lat = compute_bbox(corners_geo)
 
 # Find the top-left corner in geographic coordinates
 top_left_lon = corners_geo[0][0]
 top_left_lat = corners_geo[0][1]
 
-# Calculate the image dimensions in degrees
-image_width_degrees = image_width_meters / meters_per_degree_lon
-image_height_degrees = image_height_meters / meters_per_degree_lat
-
-# Calculate the pixel size in degrees
-pixel_size_x_degrees = image_width_degrees / image_width_pixels
-pixel_size_y_degrees = -image_height_degrees / image_height_pixels
-
-corners_pixels = [
-    (-image_width_pixels / 2.0, image_height_pixels / 2.0),  # Top-left
-    (image_width_pixels / 2.0, image_height_pixels / 2.0),   # Top-right
-    (-image_width_pixels / 2.0, -image_height_pixels / 2.0), # Bottom-left
-    (image_width_pixels / 2.0, -image_height_pixels / 2.0),  # Bottom-right
-]
-
+corners_pixels = get_corners(image_width_pixels, image_height_pixels)
 corners_pixels_rotated = rotate_corners(corners_pixels, YAW)
+img_pix_x, img_pix_y = compute_bbox(corners_pixels_rotated)
 
-max_long = max(corner[0] for corner in corners_geo)
-min_long = min(corner[0] for corner in corners_geo)
-max_lat = max(corner[1] for corner in corners_geo)
-min_lat = min(corner[1] for corner in corners_geo)
+pixel_size_long = img_long/img_pix_x
+pixel_size_lat = img_lat/-img_pix_y # Invert y axis
 
-max_pix_x = max(corner[0] for corner in corners_pixels_rotated)
-min_pix_x = min(corner[0] for corner in corners_pixels_rotated)
-max_pix_y = max(corner[1] for corner in corners_pixels_rotated)
-min_pix_y = min(corner[1] for corner in corners_pixels_rotated)
-
-print(f"min_long: {min_long:.8f}")
-print(f"max_long: {max_long:.8f}")
-print(f"min_lat: {min_lat:.8f}")
-print(f"max_lat: {max_lat:.8f}")
-print(f"min_pix_x: {min_pix_x:.8f}")
-print(f"max_pix_x: {max_pix_x:.8f}")
-print(f"min_pix_y: {min_pix_y:.8f}")
-print(f"max_pix_y: {max_pix_y:.8f}")
-
-img_long = max_long - min_long
-img_lat = max_lat - min_lat
-img_pix_x = max_pix_x - min_pix_x
-img_pix_y = min_pix_y - max_pix_y
-
+print("Geo corners: " + str(corners_geo))
 print(f"img_long: {img_long:.8f}")
 print(f"img_lat: {img_lat:.8f}")
 print(f"img_pix_x: {img_pix_x:.8f}")
 print(f"img_pix_y: {img_pix_y:.8f}")
-
-pixel_size_long = img_long/img_pix_x
-pixel_size_lat = img_lat/img_pix_y # Invert y axis
-
-print(f"Image Width (degrees): {image_width_degrees:.8f}")
 print(f"Image Width (pixels): {image_width_pixels:.8f}")
-print(f"Image Height (degrees): {image_height_degrees:.8f}")
 print(f"Image Height (pixels): {image_height_pixels:.8f}")
 print(f"Top-Left Longitude: {top_left_lon:.8f}")
 print(f"Top-Left Latitude: {top_left_lat:.8f}")
 print(f"YAW: {YAW:.8f}")
-print(f"Pixel Size (X, degrees): {pixel_size_x_degrees:.8f}")
-print(f"Pixel Size (Y, degrees): {pixel_size_y_degrees:.8f}")
 print(f"Pixel Size (Long, degrees): {pixel_size_long:.8f}")
 print(f"Pixel Size (Lat, degrees): {pixel_size_lat:.8f}")
-
-# Create the individual matrices
-# translation_matrix = Affine.translation(top_left_lon, top_left_lat)
-# rotation_matrix = Affine.rotation(-math.degrees(YAW)) # Cancels out the flipped Y axis (Mirrored image)
-# scaling_matrix = Affine.scale(pixel_size_x_degrees, pixel_size_y_degrees)
-# transform = translation_matrix * scaling_matrix * rotation_matrix
 
 translation_matrix = Affine.translation(top_left_lon, top_left_lat)
 scaling_matrix = Affine.scale(pixel_size_long, pixel_size_lat)
 rotation_matrix = Affine.rotation(-math.degrees(YAW))
-# transform = translation_matrix * rotation_matrix * scaling_matrix
 transform = translation_matrix * scaling_matrix * rotation_matrix
-
 
 # Print the individual matrices
 print("\nTranslation Matrix:")
@@ -207,7 +159,6 @@ print(f"|{transform.g:.8f}, {transform.h:.8f}, {transform.i:.8f}|")
 
 # Define the CRS (e.g., WGS84)
 crs = 'EPSG:4326'
-# crs = 'EPSG:32618'
 
 # Save the geo-referenced image as a GeoTIFF
 with rasterio.open(
