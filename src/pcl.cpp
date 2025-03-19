3e710ac6120838652cebe56380df4c4441fc6a3a
chcaya <cayc2401@usherbrooke.ca>
2025-03-19 13:23:29 -0400

    Cleaned up pcl

--- src/pcl.cpp
+++ src/pcl.cpp
@@ -62,16 +62,16 @@ void extractPoints(
     extract.filter(_outputCloud);
 }
 
-pcl::PointIndices removeNaNFromNormalCloud(pcl::PointCloud<pcl::PointNormal>& _normalsCloud){
+pcl::PointIndices removeNaNFromNormalCloud(pcl::PointCloud<pcl::PointNormal>::Ptr _normalsCloud){
     pcl::PointCloud<pcl::PointNormal> tempCloud;
-    tempCloud.points.reserve(_normalsCloud.points.size());
+    tempCloud.points.reserve(_normalsCloud->points.size());
 
     // Create a PointIndices object to store the removed indices
     pcl::PointIndices removedIndices;
-    removedIndices.indices.reserve(_normalsCloud.points.size());
+    removedIndices.indices.reserve(_normalsCloud->points.size());
 
     int i = 0;
-    for (const auto& normal : _normalsCloud.points) {
+    for (const auto& normal : _normalsCloud->points) {
         if (!std::isnan(normal.normal_x) && !std::isnan(normal.normal_y) && !std::isnan(normal.normal_z))
         {
             tempCloud.emplace_back(normal);
@@ -84,47 +84,47 @@ pcl::PointIndices removeNaNFromNormalCloud(pcl::PointCloud<pcl::PointNormal>& _n
         ++i;
     }
 
-    pcl::copyPointCloud(tempCloud, _normalsCloud);
+    pcl::copyPointCloud(tempCloud, *_normalsCloud);
     return removedIndices;
 }
 
-pcl::PointIndices computeNormalsPointCloud(
-    pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud,
-    pcl::PointCloud<pcl::PointNormal>& _normalsCloud,
-    pcl::search::KdTree<pcl::PointXYZRGB>& _kdTree,
+pcl::PointIndices computeNormalsPC(
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud,
+    pcl::PointCloud<pcl::PointNormal>::Ptr _normalsCloud,
     const pcl::PointXYZRGB& _viewPoint,
     const int _searchNeighbors
 ){
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdTree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+    kdTree->setInputCloud(_pointCloud);
+
     pcl::NormalEstimation<pcl::PointXYZRGB, pcl::PointNormal> ne;
-    ne.setInputCloud(_pointCloud.makeShared());
+    ne.setInputCloud(_pointCloud);
     ne.setViewPoint(_viewPoint.x, _viewPoint.y, _viewPoint.z);
-    ne.setSearchMethod(std::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>(_kdTree));
+    ne.setSearchMethod(kdTree);
     ne.setKSearch(_searchNeighbors);
-    ne.compute(_normalsCloud);
+    ne.compute(*_normalsCloud);
 
     pcl::PointIndices removedIdx = removeNaNFromNormalCloud(_normalsCloud);
-    extractPoints(_pointCloud, _pointCloud, removedIdx, true);
+    extractPoints(*_pointCloud, *_pointCloud, removedIdx, true);
 
     return removedIdx;
 }
 
-pcl::PointCloud<pcl::PointNormal> extractNormalsPC(
-    pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud,
+pcl::PointCloud<pcl::PointNormal>::Ptr extractNormalsPC(
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud,
     const pcl::PointXYZRGB& _centroid
 ){
-    pcl::PointCloud<pcl::PointNormal> normalsCloud;
-    pcl::search::KdTree<pcl::PointXYZRGB> kdTree;
-    kdTree.setInputCloud(_pointCloud.makeShared());
-    computeNormalsPointCloud(_pointCloud, normalsCloud, kdTree, _centroid, N_NEIGHBORS_SEARCH);
+    pcl::PointCloud<pcl::PointNormal>::Ptr normalsCloud(new pcl::PointCloud<pcl::PointNormal>);
+    computeNormalsPC(_pointCloud, normalsCloud, _centroid, N_NEIGHBORS_SEARCH);
     return normalsCloud;
 }
 
-pcl::PointXYZRGB getHighestPoint(pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud){
+pcl::PointXYZRGB getHighestPoint(pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud){
     // Initialize the highest point with the first point in the cloud
-    pcl::PointXYZRGB highestPoint = _pointCloud.points[0];
+    pcl::PointXYZRGB highestPoint = _pointCloud->points[0];
 
     // Iterate through the cloud to find the highest point
-    for (const auto& point : _pointCloud.points) {
+    for (const auto& point : _pointCloud->points) {
         if (point.z > highestPoint.z) {
             highestPoint = point;
         }
@@ -134,44 +134,49 @@ pcl::PointXYZRGB getHighestPoint(pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud)
 }
 
 void downSamplePointCloud(
-    pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud,
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud,
     const float _leafSize
 ){
     pcl::VoxelGrid<pcl::PointXYZRGB> sor;
-    sor.setInputCloud(_pointCloud.makeShared());
+    sor.setInputCloud(_pointCloud);
     sor.setLeafSize(_leafSize, _leafSize, _leafSize);
-    sor.filter(_pointCloud);
+    sor.filter(*_pointCloud);
 }
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr extractNeighborPC(
-    const pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud,
-    const pcl::search::KdTree<pcl::PointXYZRGB> _kdtree,
+    const pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud,
     const pcl::PointXYZRGB _center,
     const float _radius
 ){
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+    kdtree->setInputCloud(_pointCloud);
+
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr neighborsPC(new pcl::PointCloud<pcl::PointXYZRGB>);
-    neighborsPC->points.reserve(_pointCloud.points.size());
+    neighborsPC->points.reserve(_pointCloud->points.size());
 
     std::vector<int> pointIdxRadiusSearch;
     std::vector<float> pointRadiusSquaredDistance;
-    if(_kdtree.radiusSearch(_center, _radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0){
+    if(kdtree->radiusSearch(_center, _radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0){
         for (size_t i = 0; i < pointIdxRadiusSearch.size(); ++i){
-            neighborsPC->points.emplace_back(_pointCloud.points[pointIdxRadiusSearch[i]]);
+            neighborsPC->points.emplace_back(_pointCloud->points[pointIdxRadiusSearch[i]]);
         }
     }
 
     return neighborsPC;
 }
 
-pcl::PointIndices extractBiggestCluster(pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud, const float _threshold, const int _minPoints)
-{
+pcl::PointIndices extractBiggestCluster(
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud,
+    const float _threshold,
+    const int _minPoints
+){
     // Cluster extraction
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
     ec.setClusterTolerance(_threshold); // Adjust tolerance (distance threshold)
     ec.setMinClusterSize(_minPoints); // Adjust minimum points per cluster
     ec.setMaxClusterSize(std::numeric_limits<int>::max()); // No upper limit
-    ec.setInputCloud(_pointCloud.makeShared());
+    ec.setInputCloud(_pointCloud);
     ec.extract(cluster_indices);
 
     // Find the largest cluster
@@ -191,7 +196,7 @@ pcl::PointIndices extractBiggestCluster(pcl::PointCloud<pcl::PointXYZRGB>& _poin
     if (largest_cluster_index != -1)
     {
         inliers = cluster_indices[largest_cluster_index];
-        extractPoints(_pointCloud, _pointCloud, inliers, false);
+        extractPoints(*_pointCloud, *_pointCloud, inliers, false);
         std::cout << "The point cloud has " << std::to_string(cluster_indices.size()) << " clusters." << std::endl;
     }
     else
@@ -202,7 +207,7 @@ pcl::PointIndices extractBiggestCluster(pcl::PointCloud<pcl::PointXYZRGB>& _poin
     return inliers;
 }
 
-void smoothPC(pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud, pcl::search::KdTree<pcl::PointXYZRGB>& _kdtree, const float _searchRadius)
+void smoothPC(pcl::PointCloud<pcl::PointXYZRGB>::Ptr _pointCloud, const float _searchRadius)
 {
     // Output has the PointNormal type in order to store the normals calculated by MLS
     pcl::PointCloud<pcl::PointNormal> mls_points;
@@ -212,35 +217,43 @@ void smoothPC(pcl::PointCloud<pcl::PointXYZRGB>& _pointCloud, pcl::search::KdTre
     
     mls.setComputeNormals(true);
 
-    pcl::search::KdTree<pcl::PointXYZRGB> pointTree;
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+    kdtree->setInputCloud(_pointCloud);
     // Set parameters
-    mls.setInputCloud(_pointCloud.makeShared());
+    mls.setInputCloud(_pointCloud);
     mls.setPolynomialOrder(2);
-    mls.setSearchMethod(std::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>(_kdtree));
+    mls.setSearchMethod(kdtree);
     mls.setSearchRadius(_searchRadius);
 
     // Reconstruct
     mls.process(mls_points);
 
-    pcl::copyPointCloud(mls_points, _pointCloud);
+    // Manual copy because of different types PointNormal vs PointXYZRGB
+    for (size_t i = 0; i < mls_points.size(); ++i) {
+        _pointCloud->points[i].x = mls_points.points[i].x;
+        _pointCloud->points[i].y = mls_points.points[i].y;
+        _pointCloud->points[i].z = mls_points.points[i].z;
+    }
+
+    // pcl::copyPointCloud(mls_points, *_pointCloud);
 }
 
-float computeDensity(const pcl::PointCloud<pcl::PointXYZRGB>& _cloud, float _radius) {
+float computeDensity(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr _cloud, float _radius) {
     // Compute the volume of the sphere
     float volume = (4.0f / 3.0f) * M_PI * std::pow(_radius, 3);
 
     // Compute the density
-    float density = _cloud.points.size() / volume;
+    float density = _cloud->points.size() / volume;
 
     std::cout << "Density: " << density << std::endl;
     return density;
 }
 
 int projectPoint(
-    const pcl::PointCloud<pcl::PointXYZRGB>& _cloud,
+    const pcl::PointCloud<pcl::PointXYZRGB>::Ptr _cloud,
     pcl::PointXYZRGB& _point
 ){
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_copy(new pcl::PointCloud<pcl::PointXYZRGB>(_cloud));
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_copy(new pcl::PointCloud<pcl::PointXYZRGB>(*_cloud));
     for (auto& point : cloud_copy->points) {
         point.z = 0;
     }
@@ -254,7 +267,7 @@ int projectPoint(
     float avgZ = 0.0;
     if (kdtree->nearestKSearch(_point, nClosestPoints, point_indices, point_distances) > 0) {
         for (auto& idx : point_indices) {
-            pcl::PointXYZRGB closest_point = _cloud.points[idx];
+            pcl::PointXYZRGB closest_point = _cloud->points[idx];
             std::cout << "Closest point: (" << closest_point.x << ", "
                       << closest_point.y << ", " << closest_point.z << ")" << std::endl;
             std::cout << "Z coordinate: " << closest_point.z << std::endl;
@@ -270,12 +283,12 @@ int projectPoint(
 }
 
 pcl::PrincipalCurvatures computeCurvature(
-    const pcl::PointCloud<pcl::PointXYZRGB>& _cloud,
+    const pcl::PointCloud<pcl::PointXYZRGB>::Ptr _cloud,
     const pcl::PointXYZRGB& _point,
     const float _radius
 ){
     // Step 1: Create a copy of the input cloud and add the point
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_copy(new pcl::PointCloud<pcl::PointXYZRGB>(_cloud));
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_copy(new pcl::PointCloud<pcl::PointXYZRGB>(*_cloud));
     cloud_copy->push_back(_point);
 
     // Step 2: Create a KdTree and set the input cloud
@@ -411,10 +424,10 @@ void printPoint(const pcl::PointXYZRGB& _point){
 }
 
 void colorSegmentedPoints(
-    pcl::PointCloud<pcl::PointXYZRGB>& _coloredCloud,
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _coloredCloud,
     const pcl::RGB& _color
 ){
-    for (auto& point : _coloredCloud.points)
+    for (auto& point : _coloredCloud->points)
     {
         point.r = _color.r;
         point.g = _color.g;
@@ -423,7 +436,7 @@ void colorSegmentedPoints(
 }
 
 void colorSegmentedPoints(
-    pcl::PointCloud<pcl::PointXYZRGB>& _coloredCloud,
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr _coloredCloud,
     const pcl::PointIndices& inliers,
     const pcl::RGB& _color
 ){
@@ -432,17 +445,17 @@ void colorSegmentedPoints(
     {
         const auto& idx = inliers.indices[i];
 
-        if(_coloredCloud.points[idx].r == _coloredCloud.points[idx].g && _coloredCloud.points[idx].g == _coloredCloud.points[idx].b)
+        if(_coloredCloud->points[idx].r == _coloredCloud->points[idx].g && _coloredCloud->points[idx].g == _coloredCloud->points[idx].b)
         {
-            _coloredCloud.points[idx].r = _color.r;
-            _coloredCloud.points[idx].g = _color.g;
-            _coloredCloud.points[idx].b = _color.b;
+            _coloredCloud->points[idx].r = _color.r;
+            _coloredCloud->points[idx].g = _color.g;
+            _coloredCloud->points[idx].b = _color.b;
         }
         else
         {
-            if(_coloredCloud.points[idx].r == 0){_coloredCloud.points[idx].r = _color.r;}
-            if(_coloredCloud.points[idx].g == 0){_coloredCloud.points[idx].g = _color.g;}
-            if(_coloredCloud.points[idx].b == 0){_coloredCloud.points[idx].b = _color.b;}
+            if(_coloredCloud->points[idx].r == 0){_coloredCloud->points[idx].r = _color.r;}
+            if(_coloredCloud->points[idx].g == 0){_coloredCloud->points[idx].g = _color.g;}
+            if(_coloredCloud->points[idx].b == 0){_coloredCloud->points[idx].b = _color.b;}
         }
     }
 }
@@ -471,47 +484,42 @@ void view(const std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> _clouds){
 int main() {
     std::string ply_file_path = "../inputs/rtabmap_cloud.ply";
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud = loadPly(ply_file_path);
-    pcl::search::KdTree<pcl::PointXYZRGB> kdTree;
     // pcl::PointCloud<pcl::PointNormal> normalsCloud = extractNormalsPC(*cloud, pcl::PointXYZRGB(0.0, 0.0, 0.0, 255, 255, 255));
 
     // Landing point [15.50081099  3.76794873 12.62245941]
 
     pcl::PointXYZRGB centerPoint(15.50081099, 3.76794873, 0.0, 255, 255, 255);
-    pcl::PointXYZRGB highestPoint = getHighestPoint(*cloud);
+    pcl::PointXYZRGB highestPoint = getHighestPoint(cloud);
     centerPoint.z = highestPoint.z;
     printPoint(centerPoint);
 
-    downSamplePointCloud(*cloud, 0.1);
+    downSamplePointCloud(cloud, 0.1);
 
-    kdTree.setInputCloud(cloud);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr dfCloud = extractNeighborPC(*cloud, kdTree, centerPoint, DRONE_RADIUS);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr dfCloud = extractNeighborPC(cloud, centerPoint, DRONE_RADIUS);
 
-    pcl::PointIndices biggestIdx = extractBiggestCluster(*cloud, 0.5, 10);
+    pcl::PointIndices biggestIdx = extractBiggestCluster(cloud, 0.5, 10);
 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr smoothCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::copyPointCloud(*cloud, *smoothCloud);
-    downSamplePointCloud(*smoothCloud, 0.5);
+    downSamplePointCloud(smoothCloud, 0.5);
 
-    kdTree.setInputCloud(smoothCloud);
-    smoothPC(*smoothCloud, kdTree, 2*DRONE_RADIUS);
-    // projectPoint(*cloud, centerPoint);
-    projectPoint(*smoothCloud, centerPoint);
+    smoothPC(smoothCloud, 2*DRONE_RADIUS);
+    // projectPoint(cloud, centerPoint);
+    projectPoint(smoothCloud, centerPoint);
 
-    pcl::PointXYZRGB highestPointSmooth = getHighestPoint(*smoothCloud);
-    kdTree.setInputCloud(cloud);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr highCloud = extractNeighborPC(*cloud, kdTree, highestPointSmooth, DRONE_RADIUS);
+    pcl::PointXYZRGB highestPointSmooth = getHighestPoint(smoothCloud);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr highCloud = extractNeighborPC(cloud, highestPointSmooth, DRONE_RADIUS);
 
-    kdTree.setInputCloud(highCloud);
-    computeCurvature(*smoothCloud, centerPoint, DRONE_RADIUS);
-    computeDensity(*cloud, DRONE_RADIUS);
+    computeCurvature(smoothCloud, centerPoint, DRONE_RADIUS);
+    computeDensity(cloud, DRONE_RADIUS);
     Eigen::Vector4f coef = computePlane(highCloud);
     computePlaneAngle(coef);
     computeStandardDeviation(highCloud, coef);
 
-    colorSegmentedPoints(*cloud, pcl::RGB(255,255,255));
-    colorSegmentedPoints(*dfCloud, pcl::RGB(255,0,0));
-    colorSegmentedPoints(*smoothCloud, pcl::RGB(0,0,255));
-    colorSegmentedPoints(*highCloud, pcl::RGB(255,255,0));
+    colorSegmentedPoints(cloud, pcl::RGB(255,255,255));
+    colorSegmentedPoints(dfCloud, pcl::RGB(255,0,0));
+    colorSegmentedPoints(smoothCloud, pcl::RGB(0,0,255));
+    colorSegmentedPoints(highCloud, pcl::RGB(255,255,0));
     view(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>{cloud, smoothCloud, dfCloud, highCloud});
     return 0;
 }
